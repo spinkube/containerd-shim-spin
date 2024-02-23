518dc6b2f3c2e76262f162c5231c1306e0fb54aa
Kate Goldenring <kate.goldenring@fermyon.com>
2024-02-23 12:38:04 -0800

    fix(logging): disable logging in shim to improve performance
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- containerd-shim-spin/src/main.rs
+++ containerd-shim-spin/src/main.rs
@@ -1,8 +1,22 @@
+use containerd_shim::Config;
 use containerd_shim_wasm::container::Instance;
 use containerd_shim_wasm::sandbox::cli::{revision, shim_main, version};
 
 mod engine;
 
 fn main() {
-    shim_main::<Instance<engine::SpinEngine>>("spin", version!(), revision!(), "v2", None);
+    // Configure the shim to disable all logging for performance improvements.
+    // TODO: consider supporting some logging once log level specification is
+    // supported in https://github.com/containerd/rust-extensions/pull/247
+    let shim_config = Config {
+        no_setup_logger: true,
+        ..Default::default()
+    };
+    shim_main::<Instance<engine::SpinEngine>>(
+        "spin",
+        version!(),
+        revision!(),
+        "v2",
+        Some(shim_config),
+    );
 }
