27b4d6f689cebb73e88773f68d841647064d1462
Rajat Jindal <rajatjindal83@gmail.com>
2024-04-22 15:30:02 +0530

    setup rust/tinygo for building spin apps
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -2,3 +2,12 @@
 sudo apt -y update
 sudo apt-get install -y protobuf-compiler libseccomp-dev
 
+echo "setting up rust"
+sudo rustup toolchain install 1.74 --component clippy --component rustfmt --no-self-update
+sudo rustup default 1.74
+sudo rustup target add wasm32-wasi && rustup target add wasm32-unknown-unknown
+
+## setup tinygo. required for building test spin app
+echo "setting up tinygo"
+wget https://github.com/tinygo-org/tinygo/releases/download/v0.30.0/tinygo_0.30.0_amd64.deb
+sudo dpkg -i tinygo_0.30.0_amd64.deb
