181f6f83d635b6fc515bb9efc3198a2500619e97
Rajat Jindal <rajatjindal83@gmail.com>
2024-04-29 12:50:53 +0530

    address review feedback
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y protobuf-compiler libseccomp-dev
 echo "setting up rust"
 sudo rustup toolchain install 1.74 --component clippy --component rustfmt --no-self-update
 sudo rustup default 1.74
-sudo rustup target add wasm32-wasi && rustup target add wasm32-unknown-unknown
+sudo rustup target add wasm32-wasi && sudo rustup target add wasm32-unknown-unknown
 
 ## setup tinygo. required for building test spin app
 echo "setting up tinygo"
