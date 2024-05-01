7e234fd4172258f45362f60b39925b4288cb8392
Rajat Jindal <rajatjindal83@gmail.com>
2024-05-01 16:19:09 +0530

    use default rust version from toolchain config
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -3,8 +3,7 @@ sudo apt -y update
 sudo apt-get install -y protobuf-compiler libseccomp-dev
 
 echo "setting up rust"
-sudo rustup toolchain install 1.74 --component clippy --component rustfmt --no-self-update
-sudo rustup default 1.74
+sudo rustup toolchain install --component clippy --component rustfmt --no-self-update
 sudo rustup target add wasm32-wasi && sudo rustup target add wasm32-unknown-unknown
 
 ## setup tinygo. required for building test spin app
