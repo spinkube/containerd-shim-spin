e658965d40baba4e20a4a0a8647e7c5c042e55cf
Kate Goldenring <kate.goldenring@fermyon.com>
2024-07-30 16:55:13 -0700

    Consolidate two redis trigger test apps
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- images/spin-multi-trigger-app/spin-redis-trigger/src/lib.rs
+++ images/spin-multi-trigger-app/spin-redis-trigger/src/lib.rs
@@ -1,16 +1,20 @@
-use anyhow::{Result, anyhow};
-use bytes::Bytes;
-use spin_sdk::redis_component;
 use std::str::from_utf8;
-use spin_sdk::{redis, variables};
 
-/// A simple Spin Redis component.
+use anyhow::{anyhow, Result};
+use bytes::Bytes;
+use spin_sdk::{redis, redis_component, variables};
+
 #[redis_component]
 fn on_message(message: Bytes) -> Result<()> {
-    let conn = redis::Connection::open("redis://redis-service.default.svc.cluster.local:6379")?;
+    let address = variables::get("redis_address").expect("could not get variable");
+    let conn = redis::Connection::open(&address)?;
+
     println!("{}", from_utf8(&message)?);
 
-    conn.set("spin-multi-trigger-app-key", &"spin-multi-trigger-app-value".to_owned().into_bytes())
+    conn.set(
+        "redis-trigger-app-key",
+        &"redis-trigger-app-value".to_owned().into_bytes(),
+    )
     .map_err(|_| anyhow!("Error executing Redis set command"))?;
 
     Ok(())
