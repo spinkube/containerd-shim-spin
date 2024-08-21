f8cd881ef980f296ca5ebfacc1003be4282bc5c0
Kate Goldenring <kate.goldenring@fermyon.com>
2024-08-21 14:34:13 -0700

    Use cluster IP for MQTT broker address for Spin app trigger
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- scripts/deploy-workloads.sh
+++ scripts/deploy-workloads.sh
@@ -9,11 +9,21 @@ if ! command -v kubectl &> /dev/null; then
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl;
 fi
 
+update_mqtt_workload_with_broker_cluster_ip() {
+    # The MQTT trigger cannot do DNS resolution, so we need to use the IP address of the MQTT broker
+    # Replace "EMQX_CLUSTER_IP" with the actual ClusterIP of the EMQX service
+    local cluster_ip=$(kubectl get svc emqx -o jsonpath='{.spec.clusterIP}')
+    sed -i "s/EMQX_CLUSTER_IP/$cluster_ip/g" tests/workloads-pushed-using-spin-registry-push/workloads.yaml
+    echo "Updated workloads.yaml with ClusterIP: $cluster_ip"
+}
+
+
 # apply the workloads
 echo ">>> apply workloads"
 kubectl apply -f tests/workloads-common
 
 if [ "$1" == "workloads-pushed-using-spin-registry-push" ]; then
+    update_mqtt_workload_with_broker_cluster_ip
     echo "deploying spin apps pushed to registry using 'spin registry push' command"
     kubectl apply -f tests/workloads-pushed-using-spin-registry-push
 else
@@ -38,3 +48,4 @@ kubectl describe deployments
 echo ">>> Services:"
 kubectl get services -o wide
 kubectl describe services
+
