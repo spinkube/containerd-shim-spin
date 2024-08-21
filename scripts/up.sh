32c8e9feb10c94e35a35440bf76ed34ecea64dbe
Kate Goldenring <kate.goldenring@fermyon.com>
2024-08-21 12:35:43 -0700

    Add MQTT logging app to prepared apps
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 cluster_name="test-cluster"       # name of the k3d cluster
 dockerfile_path="deployments/k3d" # path to the Dockerfile
 
-DOCKER_IMAGES=("spin" "spin-keyvalue" "spin-outbound-redis" "spin-multi-trigger-app" "spin-static-assets")
+DOCKER_IMAGES=("spin" "spin-keyvalue" "spin-outbound-redis" "spin-multi-trigger-app" "spin-static-assets" "spin-mqtt-message-logger")
 OUT_DIRS=("test/out_spin" "test/out_spin_keyvalue" "test/out_spin_outbound_redis" "test/out_spin_multi_trigger_app" "test/out_spin_static_assets")
-IMAGES=("spin-hello-world" "spin-keyvalue" "spin-outbound-redis" "spin-multi-trigger-app" "spin-static-assets")
+IMAGES=("spin-hello-world" "spin-keyvalue" "spin-outbound-redis" "spin-multi-trigger-app" "spin-static-assets" "spin-mqtt-message-logger")
 
 # build the Docker image for the k3d cluster
 docker build -t k3d-shim-test "$dockerfile_path"
@@ -20,10 +20,12 @@ kubectl wait --for=condition=ready node --all --timeout=120s
 
 # Iterate through the Docker images and build them
 for i in "${!DOCKER_IMAGES[@]}"; do
-  docker buildx build -t "${IMAGES[$i]}:latest" "./images/${DOCKER_IMAGES[$i]}" --load
-  mkdir -p "${OUT_DIRS[$i]}"
-  docker save -o "${OUT_DIRS[$i]}/img.tar" "${IMAGES[$i]}:latest"
-  k3d image import "${OUT_DIRS[$i]}/img.tar" -c "$cluster_name"
+  if [ -f "./images/${DOCKER_IMAGES[$i]}/Dockerfile" ]; then
+    docker buildx build -t "${IMAGES[$i]}:latest" "./images/${DOCKER_IMAGES[$i]}" --load
+    mkdir -p "${OUT_DIRS[$i]}"
+    docker save -o "${OUT_DIRS[$i]}/img.tar" "${IMAGES[$i]}:latest"
+    k3d image import "${OUT_DIRS[$i]}/img.tar" -c "$cluster_name"
+  fi
 
   ## also do spin builds and spin registry push
   ## images pushed as localhost:5000/<namespace>/<app>:<version>
