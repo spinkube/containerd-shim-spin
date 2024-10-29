765a74e2e730ac47b720fa1bc598554f5e292ba7
Kate Goldenring <kate.goldenring@fermyon.com>
2024-10-28 20:22:45 -0700

    Update Spin and Go versions for actions
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -8,6 +8,6 @@ sudo rustup target add wasm32-wasi && sudo rustup target add wasm32-unknown-unkn
 
 ## setup tinygo. required for building test spin app
 echo "setting up tinygo"
-wget https://github.com/tinygo-org/tinygo/releases/download/v0.30.0/tinygo_0.30.0_amd64.deb
-sudo dpkg -i tinygo_0.30.0_amd64.deb
-rm tinygo_0.30.0_amd64.deb
+wget https://github.com/tinygo-org/tinygo/releases/download/v0.34.0/tinygo_0.34.0_amd64.deb
+sudo dpkg -i tinygo_0.34.0_amd64.deb
+rm tinygo_0.34.0_amd64.deb
