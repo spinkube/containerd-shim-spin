ed7911779f502f0fcdc39ea3de76da5cf5ec71b3
Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>
2024-12-16 20:11:04 +0000

    refactor(k3s): relocate and update k3s installation script
    Moved install-k3s.sh from tests/k3s to scripts directory
    * Updated install-k3s.sh script to set KUBECONFIG directly to /etc/rancher/k3s/k3s.yaml
    
    Signed-off-by: Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>

--- scripts/install-k3s.sh
+++ scripts/install-k3s.sh
@@ -7,11 +7,8 @@ set -euo pipefail
 # We will just need to make sure that containerd-shim-spin-v2 binary is in PATH
 # and that the k3s service is running.
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode '0644' 
 sudo systemctl start k3s
-sudo k3s kubectl get nodes
 
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/k3s.yaml
-export KUBECONFIG=$HOME/.kube/k3s.yaml
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 kubectl get nodes
